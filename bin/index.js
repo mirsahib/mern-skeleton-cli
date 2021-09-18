5fec3f474a79e3700d78d2dff00b7417d0a9e475
mirsahib <mirsahib24@gmail.com>
2021-09-18 23:22:16 +0600

    :contruction: working on nested command structure

--- bin/index.js
+++ bin/index.js
@@ -1,27 +1,15 @@
 #! /usr/bin/env node
-const { program } = require('commander');
-const pj = require('../package.json')
-
-program.version(pj.version)
-//commands
-program
-      .command('create')
-      .description("Create a new project")
-      .usage('-p | --project [<project name>]')
-      .option('-p, --project <optional>','project name')
-      .option('-c, --controller <required>','create controller')
-      .action(function (args) {
-            //console.log('mern created',args.project)
-            // if(args.project===undefined){
-            //       console.log('creating project mern-skeleton')
-            // }else{
-            //       console.log(`creating project ${args.project}`)
-            // }
-            if(Object.keys(args).length<=1){
-                  console.log('creating project mern-skeleton')
-            }else{
-                  console.log("error: argument overload, expected one argument at a time")
-            }
-            //console.log(Object.keys(args).length)
-      })
-      .parse(process.argv)
+const commander = require('commander');
+const program = new commander.Command();
+const mern = program.command('create');
+mern
+      .command('project')
+      .action(() => {
+            console.log('create project');
+      });
+mern
+      .command('controller')
+      .action(() => {
+            console.log('create controller');
+      });
+program.parse(process.argv);
