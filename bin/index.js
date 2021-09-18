5148759d0867c2b1add8fd822f993934a8510d08
mirsahib <mirsahib24@gmail.com>
2021-09-18 22:19:14 +0600

    :construction: working with commander.js

--- bin/index.js
+++ bin/index.js
@@ -1,26 +1,27 @@
 #! /usr/bin/env node
-const yargs = require("yargs");
-const utils = require("./utils");
-const argv = yargs.argv
+const { program } = require('commander');
+const pj = require('../package.json')
 
-const usage = "\nUsage: mern <command> [options]";
-
-const options = yargs
-      .command("create", "<Project Name> Create a new mern project",
-            {
-                  project:
-                        { describe: "Name of the project", alias: 'p',demandOption:true }
+program.version(pj.version)
+//commands
+program
+      .command('create')
+      .description("Create a new project")
+      .usage('-p | --project [<project name>]')
+      .option('-p, --project <optional>','project name')
+      .option('-c, --controller <required>','create controller')
+      .action(function (args) {
+            //console.log('mern created',args.project)
+            // if(args.project===undefined){
+            //       console.log('creating project mern-skeleton')
+            // }else{
+            //       console.log(`creating project ${args.project}`)
+            // }
+            if(Object.keys(args).length<=1){
+                  console.log('creating project mern-skeleton')
+            }else{
+                  console.log("error: argument overload, expected one argument at a time")
             }
-      )
-      .help()
-      .alias('help', 'h')
-      .argv
-
-// show help
-if (options._[0] == null) {
-      utils.showHelp();
-      return;
-}else{
-      utils.createProject(options)
-}
-
+            //console.log(Object.keys(args).length)
+      })
+      .parse(process.argv)
