752f054763c359892799f4d5341c737e28329e39
mirhabibul.latif@techhack.ca <mirsahib24@gmail.com>
2021-09-21 15:52:25 +0600

    :recycle: formate code

--- bin/index.js
+++ bin/index.js
@@ -16,23 +16,23 @@ program
 
 program.command('create')
       .option('-p, --project <name>', 'Create a new mern project')
-      .option('-c, --controller <name>','Create a new controller')
-      .option('-r, --router <name>',"Create a new router")
-      .action((args)=>{
+      .option('-c, --controller <name>', 'Create a new controller')
+      .option('-r, --router <name>', "Create a new router")
+      .action((args) => {
             //allow only one argument
             let argsLength = Object.keys(args).length
-            if(argsLength<=1){
-                  if(args.project){
+            if (argsLength <= 1) {
+                  if (args.project) {
                         util.createProject(args.project)
-                  }else if(args.controller){
+                  } else if (args.controller) {
                         util.createProject(args.controller)
-                  }else if(args.router){
+                  } else if (args.router) {
                         util.createProject(args.router)
-                  }else{
+                  } else {
                         log(chalk.red.bold('error: Something went wrong'))
                   }
 
-            }else{
+            } else {
                   log(chalk.red.bold('error: Multiple argument has been passed only one argument is allowed'))
             }
       })
